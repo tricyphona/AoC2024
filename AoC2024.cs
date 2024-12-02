9cf9089d330d1b0e798d374da8ed7403f14a0fc0
Robbert Lapoutre <robbert_lapoutre@hotmail.com>
2024-12-02 22:25:51 +0100

    Refactor AoC2024 class and update framework to v4.8
    - Added `using System.Linq;` directive to `AoC2024.cs`.
    - Changed `AoC2024` class to an abstract internal class.
    - Modified `Solve` method to be abstract in `AoC2024` class.
    - Updated target framework in `AoC2024.csproj` from `v4.7.2` to `v4.8`.
    - Added empty `<TargetFrameworkProfile />` element in `AoC2024.csproj`.
    - Fixed XML declaration formatting in `App.config`.
    - Updated `supportedRuntime` in `App.config` from `v4.7.2` to `v4.8`.

--- AoC2024.cs
+++ AoC2024.cs
@@ -6,13 +6,13 @@ using System.Threading.Tasks;
 
 namespace AoC2024
 {
-    internal class AoC2024
+    abstract internal class AoC2024
     {
         public string[] ReadPuzzleInput(string path)
         {
             return System.IO.File.ReadAllLines(path);
             
         }
-        public void Solve() { }
+        abstract public void Solve();
     }
 }
