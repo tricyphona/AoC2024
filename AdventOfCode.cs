a8be9e12c465d34cd67b4a29006436a869d12ada
Robbert Lapoutre <robbert_lapoutre@hotmail.com>
2024-12-07 14:24:33 +0100

    Day 7 solution

--- AdventOfCode.cs
+++ AdventOfCode.cs
@@ -23,8 +23,10 @@ namespace AoC2024
             //Console.ReadLine();
             //Day5 day5 = new Day5();
             //day5.Solve();
-            Day6 day6 = new Day6();
-            day6.Solve();
+            //Day6 day6 = new Day6();
+            //day6.Solve();
+            Day7 day7 = new Day7();
+            day7.Solve();
             stopwatch.Stop();
             Console.WriteLine(stopwatch.ElapsedMilliseconds);
         }
---
+++ Day7.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC2024
+{
+    internal class Day7 : AoC2024
+    {
+        public override void Solve()
+        {
+            Int64 sumSolutions = 0;
+            string[] puzzleInput = this.ReadPuzzleInput("C:\\projects\\AoC2024\\input\\dag_7.txt");
+            foreach (string line in puzzleInput)
+            {
+                Int64 number;
+                Int64 solution;
+                
+                string[] stuff = line.Split(':');
+                Int64.TryParse(stuff[0].Trim(), out solution);
+                Console.WriteLine(solution);
+                string[] options = stuff[1].Split(' ');
+                LinkedList<Int64> values = new LinkedList<Int64>();
+                foreach (string option in options)
+                {
+                    Int64.TryParse(option, out number);
+                    if (number != 0)
+                    {
+                        values.AddLast(number);
+                    }
+
+                }
+                LinkedList<Int64> possibleValues = new LinkedList<Int64>();
+                foreach (Int64 value in values)
+                {
+                    if (possibleValues.First == null) {
+                        possibleValues.AddLast(value);
+                    }
+                    else
+                    {
+                        LinkedList<Int64> tempList = new LinkedList<Int64>();
+                        foreach (Int64 possibleValue in possibleValues)
+                        {
+                            tempList.AddLast(possibleValue + value);
+                            tempList.AddLast(possibleValue * value);
+                            tempList.AddLast(Int64.Parse(possibleValue.ToString() + value.ToString()));
+
+                        }
+                        possibleValues = tempList;
+                    }
+                }
+                if (possibleValues.Contains(solution))
+                {
+                    Console.WriteLine("Solution found");
+                    sumSolutions += solution;
+                }
+            }
+            Console.WriteLine(sumSolutions);
+            Console.ReadLine();
+        }
+        
+    }
+}
