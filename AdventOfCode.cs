8077f584e0c013488b6c31d1f6f077c3a54e9323
tricyphona <robbert_lapoutre@hotmail.com>
2024-12-09 20:25:59 +0100

    dag9

--- AdventOfCode.cs
+++ AdventOfCode.cs
@@ -25,8 +25,10 @@ namespace AoC2024
             //day5.Solve();
             //Day6 day6 = new Day6();
             //day6.Solve();
-            Day7 day7 = new Day7();
-            day7.Solve();
+            //Day7 day7 = new Day7();
+            //day7.Solve();
+            Day9 day9 = new Day9();
+            day9.Solve();
             stopwatch.Stop();
             Console.WriteLine(stopwatch.ElapsedMilliseconds);
         }
